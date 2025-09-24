762e081e4376b3ec5a512ced6d6aca88367a4685
wassi <wassimaarab18@gmail>
2025-09-24 20:04:39 +0200

    Refactor ShoppingCart class to use Spanish variable names and improve total calculation logic

--- src/ShoppingCart.java
+++ src/ShoppingCart.java
@@ -1,22 +1,22 @@
 import java.util.HashMap;
 
 public class ShoppingCart {
-    private HashMap<String, Integer> cart;
-    private HashMap<String, Double> prices;
+    private HashMap<String, Integer> carrito;
+    private HashMap<String, Double> tienda;
 
     public ShoppingCart() {
-        this.cart = new HashMap<>();
-        this.prices = new HashMap<>();
-        this.prices.put(" manzana ", 0.50);
-        this.prices.put(" platano ", 0.30);
-        this.prices.put(" naranja ", 0.60);
-        this.prices.put(" leche ", 2.50);
-        this.prices.put(" pan", 1.50);
+        this.carrito = new HashMap<>();
+        this.tienda = new HashMap<>();
+        this.tienda.put(" manzana ", 0.50);
+        this.tienda.put(" platano ", 0.30);
+        this.tienda.put(" naranja ", 0.60);
+        this.tienda.put(" leche ", 2.50);
+        this.tienda.put(" pan", 1.50);
     }
 
     public void addProduct(String product, int cantida) {
-        if (prices.containsKey(product)) {
-            cart.put(product, cantida);
+        if (tienda.containsKey(product)) {
+            carrito.put(product, cantida);
         } else {
             System.out.print("El producto no existe\n");
         }
@@ -24,8 +24,8 @@ public class ShoppingCart {
 
 
     public void eliminarProduct(String product) {
-        if (cart.containsKey(product)) {
-            cart.remove(product);
+        if (carrito.containsKey(product)) {
+            carrito.remove(product);
             System.out.printf("El producto %s eliminado\n", product);
         } else {
             System.out.print("El producto no esta en el carrito\n");
@@ -33,13 +33,13 @@ public class ShoppingCart {
     }
 
     public void updateCantidad(String product, int cantidad) {
-        if (cart.containsKey(product)) {
-            int cantidadActual = cart.get(product);
+        if (carrito.containsKey(product)) {
+            int cantidadActual = carrito.get(product);
             if (cantidad <= 0) {
-                cart.remove(product);
+                carrito.remove(product);
 
             } else {
-                cart.put(product, cantidad);
+                carrito.put(product, cantidad);
                 System.out.printf("El producto %s se ha actulizado a %d\n", product, cantidad);
             }
         } else {
@@ -50,20 +50,24 @@ public class ShoppingCart {
     public void mostrarCarrito(){
         System.out.println("Contenido del carrito :");
         System.out.println("----------------------------------");
-        double total = 0.0;
-        for (String product : cart.keySet()) {
-            total = total + prices.get(product);
-            System.out.printf("%s - Cantidad: %d - Precio: $ %.2f ", product, cart.get(product), prices.get(product));
+
+        for (String product : carrito.keySet()) {
+            System.out.printf("%s - Cantidad: %d - Precio: $ %.2f ", product, carrito.get(product), tienda.get(product));
         }
 
         System.out.printf("Total : %.2f\n", total);
 
     }
 
-    public void calcularTotal(){
-
-
-
+    public double calcularTotal(){
+        double total = 0.0;
+        for (String product : carrito.keySet()) {
+            total = total + tienda.get(product);
+        }
+        if(total > 20.0){
+            total = total * 0.90;
+        }
+     return total;
     }
 
 
